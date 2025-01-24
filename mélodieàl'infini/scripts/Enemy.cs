dfec5e55ab0bbb7ea44534374e830792e81ab3c7
AdamVignolles <adam.vignolles@gmail.com>
2025-01-24 16:17:00 +0100

    enemy

--- mélodieàl'infini/scripts/Enemy.cs
+++ mélodieàl'infini/scripts/Enemy.cs
@@ -6,41 +6,59 @@ public partial class Enemy : CharacterBody2D
 {
 	CharacterBody2D player = null;
 
-	[Export] float speed = 100.0f;
+	[Export] float speed = 200.0f;
 
 	bool stupid;
+
+	int repeat_movement = 300;
+	int current_repeat = 0;
+
+	Vector2 ScreenSize;
 
 	public override void _Ready(){
 		// chose randomly if the enemy will be stupid or not
 		RandomNumberGenerator rng = new RandomNumberGenerator();
 		stupid = rng.RandiRange(0, 1) == 0;
 
-
-
 		player = GetParent().GetNode<CharacterBody2D>("Player");
 
-		//afficher la position du joueur
-		GD.Print(player.GlobalPosition);
 
-		if (stupid){
-			speed = 50.0f;
-		}
-		else{
-			speed = 100.0f;
-		}
+		ScreenSize = GetViewportRect().Size;
 	}
 
 	public override void _Process(double delta){
-		// move to the player
-		if (player == null){
-			return;
+
+		if (stupid){
+			if (player == null){
+				return;
+			}
+			else{
+				Vector2 direction = player.GlobalPosition - GlobalPosition;
+				direction = direction.Normalized();
+				Velocity = direction * speed;
+				MoveAndSlide();
+			}
 		}
 		else{
-			Vector2 direction = player.GlobalPosition - GlobalPosition;
-			direction = direction.Normalized();
-			Velocity = direction * speed;
-			MoveAndSlide();
+			// chose a random direction
+			if (current_repeat == 0){
+				RandomNumberGenerator rng = new RandomNumberGenerator();
+				Vector2 direction = new Vector2(rng.RandfRange(-1.0f, 1.0f), rng.RandfRange(-1.0f, 1.0f));
+				direction = direction.Normalized();
+				Velocity = direction * speed;
+				MoveAndSlide();
+				current_repeat = repeat_movement;
+			}
+			else{
+				current_repeat--;
+				MoveAndSlide();
+			}
 		}
+
+		if (GlobalPosition.DistanceTo(player.GlobalPosition) < 10){
+			player.QueueFree();
+		}
+		
 
 	}
 }
